c5f3d9138847599d895a800edefc437994f115a7
Walter Sebastian Carrera <cwalter1994@gmail.com>
2020-03-24 13:26:42 -0400

    Adding sass styling

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,27 @@
 <template>
-  <b-row>
-    <b-col cols="2" align="center" v-for="{ movie_id, movie_cover, movie_title, movie_year } in movies" :key="movie_id">
-      <div class="crop">
-        <img :src="'/api/images/' + movie_cover" v-bind:alt="movie_title">
-      </div>
-      <p>{{ movie_title }}<br>{{ movie_year }}</p>
-    </b-col>
-  </b-row>
+  <div>
+    <h4 class=title>Movies</h4>
+    <b-row>
+      <b-col cols="2" align="center" v-for="{ movie_id, movie_cover, movie_title, movie_synopsis, movie_trailer } in movies" :key="movie_id">
+        <!-- <div class="crop">
+          <img :src="'/api/images/' + movie_cover" v-bind:alt="movie_title">
+        </div>
+        <p>{{ movie_title }}<br>{{ movie_year }}</p> -->
+        <b-card overlay :title="movie_title" v-bind:img-src="'/api/images/' + movie_cover" v-bind:img-alt="movie_title">
+          <b-card-text>
+            {{ movie_synopsis }}
+          </b-card-text>
+          <b-button v-b-modal="movie_id" variant="primary">
+            Watch now
+          </b-button>
+
+          <b-modal :id="movie_id" :title="movie_title">
+            <iframe width="100%" height="315" v-bind:src="movie_trailer" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </b-modal>
+        </b-card>
+      </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
@@ -53,6 +68,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+  .title {
+    padding: 10px 0;
+  }
+
   .row {
     margin-top: 2.5vh;
 
@@ -65,9 +84,26 @@ export default {
       margin-bottom: 1vh;
     }
 
+    .card-img-overlay {
+      opacity: 0;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+
+    h4 {
+      color: white;
+    }
+
     p {
+      color: white;
       text-align: left;
       font-weight: 600;
+    }
+
+    .card-body {
+      background-color: rgba(0,0,0,0.7);
     }
   }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,63 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <b-row class="splashback">
+        <b-col cols="4" class="leftsplash">
+            <b-row>
+                <b-col class="align-self-end">
+                    <img src="images/logo.png">
+                    <h2>Where Movies Meet Music</h2>
+
+                    <b-form-group label="Email address" label-for="exampleInputEmail1">
+                        <b-form-input type="email" id="exampleInputEmail1" required placeholder="Enter email">
+                        </b-form-input>
+                    </b-form-group>
+
+                    <b-form-group label="Password" label-for="exampleInputPassword1">
+                        <b-form-input type="password" id="exampleInputPassword1" required placeholder="Enter password">
+                        </b-form-input>
+                    </b-form-group>
+                    <br>
+                    <b-button type="submit" variant="primary" class="submit">Submit</b-button>
+                    <br>
+                    <br>
+                    <b-button type="button" variant="primary" class="signup">Sign Up</b-button>
+                </b-col>
+            
+            </b-row>
+        </b-col>
+        <div class="col-sm-8 rightsplash">
+            
+        </div>
+    </b-row>
 </template>
+
+<style scoped lang="scss">
+  .row {
+    margin-left: 0;
+    margin-right: 0;
+  }
+
+  .splashback{
+    height: 100vh;
+    text-align: center;
+
+    .leftsplash{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: rgba(0,0,0,0.7);
+      color: white;
+
+      img {
+        margin-bottom: 15px;
+      }
+    }
+
+    .rightsplash{
+      background-image: url(/images/Bg.jpg);
+      background-position: center;
+      background-size: cover;
+    }
+  }
+
+
+</style>
