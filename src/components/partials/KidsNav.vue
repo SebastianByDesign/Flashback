1cd2a5e0743c350bba39da1fd4fcc8738a273c92
Jonathan Teves <j.teves07@gmail.com>
2020-04-11 14:37:59 -0400

    changed /About to /Login, Added Kids page - Kids nav

---
+++ src/components/partials/KidsNav.vue
@@ -0,0 +1,168 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-container>
+
+        <b-navbar-brand href="#">Flashback Kids</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2 hdn" placeholder="Search titles or people"></b-form-input>
+              <div class="h4 mb-0">
+                <b-icon icon="search" v-on:click="showSearch"></b-icon>
+              </div>
+            </b-nav-form>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+
+            <b-nav-item-dropdown text="Genres" right>
+              <div class="row">
+                <b-dropdown-item class="all">
+                  <a v-on:click="showAll()">All</a>
+                  <!-- <a>All</a> -->
+                </b-dropdown-item>
+                <b-dropdown-item class="genre" v-for="{ genre_id, genre_name } in genres" :key="genre_id">
+                  <a :class="genre_name" v-on:click="showByGenre(genre_name)">{{ genre_name }}</a>
+                  <!-- <a :class="genre_name">{{ genre_name }}</a> -->
+                </b-dropdown-item>
+              </div>
+            </b-nav-item-dropdown>
+
+            <b-nav-item href="#" active>Movies</b-nav-item>
+            <b-nav-item href="#">Music</b-nav-item>
+
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template v-slot:button-content>
+                <b-icon icon="person-fill"></b-icon>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import { BootstrapVue, BIcon, BIconSearch, BIconPersonFill } from 'bootstrap-vue'
+
+Vue.use(VueAxios, axios, BootstrapVue)
+
+Vue.component('BIcon', BIcon)
+Vue.component('BIconSearch', BIconSearch)
+Vue.component('BIconPersonFill', BIconPersonFill)
+
+import { serverBus } from '../../main';
+
+export default {
+  name: 'KidsPage',
+  data: function() {
+    return {
+      genres: null,
+      movies: null
+    };
+  },
+  created: function() {
+    Vue.axios.get('/api/admin/movie_genres.php').then(
+      response => {
+        //console.log(response);
+        this.genres = response.data;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  },
+  methods: {
+    showSearch: function() {
+      document.querySelector('.form-control-sm').classList.toggle('hdn');
+    },
+    showAll() {
+      const dropdown = document.querySelectorAll('.genre a');
+
+      dropdown.forEach(function(item){
+        item.classList.remove('active');
+      });
+      
+      document.querySelector('.all a').classList.add('active');
+
+      Vue.axios.get('/api/index.php').then(
+        response => {
+          this.movies = response.data;
+          serverBus.$emit('showAll()', this.movies)
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    },
+    showByGenre(genre) {
+      const dropdown = document.querySelectorAll('.genre a');
+
+      dropdown.forEach(function(item){
+        item.classList.remove('active');
+      });
+
+      document.querySelector('.all a').classList.remove('active');
+      
+      document.querySelector('.'+genre).parentElement.classList.add('active');
+
+      Vue.axios.get('/api/index.php?filter='+genre).then(
+        response => {
+          this.movies = response.data;
+          serverBus.$emit('showByGenre(genre)', this.movies)
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+
+.form-control-sm {
+  transition: all .3s ease-in-out;
+  width: 200px;
+
+  &.hdn {
+    width: 0;
+    padding: 0;
+    margin: 0;
+    border: none;
+  }
+}
+
+.bi-search {
+  color: white;
+}
+
+.bi-person-fill {
+  font-size: 1.5em !important;
+}
+
+.row {
+  width: 290px;
+  padding: 0 5px;
+  margin-right: 0;
+  margin-left: 0;
+
+  li {
+    width: 140px;
+  }
+}
+
+</style>
---
+++ src/views/Kids.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="kids">
+    <KidsNav />
+    <b-container>
+      <KidsPage />
+    </b-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import KidsPage from '@/components/KidsPage.vue';
+import KidsNav from '@/components/partials/KidsNav.vue';
+
+export default {
+  name: 'Kids',
+  components: {
+    KidsPage,
+    KidsNav
+  }
+}
+</script>
